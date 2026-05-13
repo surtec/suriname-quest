997eabccceb67a9053d2a7f610c98a23144b72a4
surtec <abdoels334@gmail.com>
2026-05-13 20:29:59 -0300

    Set up GitHub Pages deployment via GitHub Actions

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/suriname-quest/',
   plugins: [react()],
   // Phaser heeft dit nodig
   define: {
